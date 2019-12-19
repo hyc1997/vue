8309bee85aabd76bacf22d2673b4db4da2ce0b1d
hyc <865326685@qq.com>
2019-12-19 22:50:49 +0800

    day06_1 2019年12月19日22:50:46

---
+++ day06/webpack-study/src/main.js
@@ -0,0 +1,8 @@
+// 1.先初始化 npm init -y 根目录会出现 package.json文件
+// 2.打包路径 webpack .\src\main.js -o  .\dist\bundle.js  webpack 需要加-o
+// 3.自动打包  本地安装 cnpm i webpack-dev-server -D 事实打包
+		// 3.1 在根目录新建webpack.config.js  提示会安装webpack 文件
+		// 3.2 在webpack 配置文件中构建路径 
+		// 3.3 在配置文件中指定文件的入口和出口
+
+console.log("ok")
